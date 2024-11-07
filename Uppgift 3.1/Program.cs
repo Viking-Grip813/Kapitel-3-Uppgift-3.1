8b92450a5e7db51525659835c3321f43dc14bbae
vist07001 <vist07001@E-PW05CGR8>
2024-11-07 12:22:59 +0100

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Uppgift3_1
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int år = int.Parse(Console.ReadLine());
+            if (år < 16 )
+            {
+                Console.WriteLine("Du är för ung för att delta");
+            }
+            else if (år == 16)
+            {
+                Console.WriteLine("Du är gammal nog att delta");
+            }
+            else if (år == 17)
+            {
+                Console.WriteLine("Du är gammal nog att delta");
+            }
+            else if (år == 18)
+            {
+                Console.WriteLine("Du är gammal nog att delta");
+            }
+            else if (år == 19)
+            {
+                Console.WriteLine("Du är gammal nog att delta");
+            }
+            else if (år > 19)
+            {
+                Console.WriteLine("Du är för gammal för att delta");
+            }
+            
+        }
+    }
+}
